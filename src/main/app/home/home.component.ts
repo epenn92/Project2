9f505ddbbd60b0c5902d922a4215bff0957e8088
Eric Penn <ep00959@georgiasouthern.edu>
2021-07-14 16:21:41 -0400

    b4 pull

--- src/main/app/home/home.component.ts
+++ src/main/app/home/home.component.ts
@@ -7,13 +7,23 @@ import { ApiService } from '../api.service';
   styleUrls: ['./home.component.css']
 })
 
+// export class HomeComponent {
+
+//   products: any[] = [];
+//   constructor(private apiService: ApiService) { }
+//   ngOnInIt() {
+//     // this.products = this.apiService.get();
+//   }
+// }
+
 export class HomeComponent implements OnInit {
-
-  products: any[] = [];
-  constructor(private apiService: ApiService) { }
-  ngOnInIt() {
-    this.products = this.apiService.
-  }
+	products = [];
+	constructor(private apiService: ApiService) { }
+	ngOnInit() {
+		this.apiService.get().subscribe((data: any=> {  
+			console.log(data);  
+			this.products = data;  
+		})  
+	}
 }
-
 
